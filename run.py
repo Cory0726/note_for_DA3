d80abea0437a6707b37c75fb23114bc07889d4b9
Cory0726 <lkfu520@gmail.com>
2025-11-26 12:04:00 +0800

    update

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import glob, os, torch
 from depth_anything_3.api import DepthAnything3
 
-def main():
+def da3_model_initial():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = DepthAnything3.from_pretrained('depth-anything/da3metric-large')
-    print(model)
+    model = model.to(device)
+    return model
 
 
 if __name__ == '__main__':
-    main()
+    # Initialize the DA3 model
+    model = da3_model_initial()
+    
